0c12b6d99b98ddc188818585cbbd7ab82498fba3
Josiah Brown <josiah.alen.brown@gmail.com>
2022-07-15 13:53:01 -0500

    Added qrcode creation script

---
+++ qrcode.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# ___________________________________________
+# This script requires the package "qrencode"
+# 
+# `sudo apt install qrencode`
+# qrencode --help
+# 
+# Make sure to give a:
+# 	Output file name
+# 	URL/path-to-file/text that you want to convert
+# __________________________________________________
+
+read -p "QR code file name: " name
+read -p "What do you want to transform?: " link
+
+qrencode -o "$name" "$link" 
+
+if [ -f "$name" ]; then
+	echo "$name created successfully"
+fi
+
+#while true; do
+
+#read -p "Do you want to move the file?: (y/n) " yn
+
+#case $yn in
+#	[yY] ) read -p "Path to new location: " path;
+#		mv $name $path && echo "$name moved to $path";
+#		break;;
+#	[nN] ) echo "exiting...";
+#		exit;;
+#	* ) echo "Invalid response";;
+#esac
+
+#done
